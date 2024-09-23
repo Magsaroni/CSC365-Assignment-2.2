bd1890b00cea2dfac1ac1c714c70f1279bf8548e
Mags <144168396+Magsaroni@users.noreply.github.com>
2024-09-22 22:09:31 -0700

    Add files via upload

---
+++ INDEX.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mars Tourism - Magaly Valdez</title>
+    <link rel="stylesheet" href="main.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <!-- The header. -->
+    <header>
+
+    <h1>WELCOME TO MARS (Tourisim)</h1>
+
+    </header>
+
+    <!-- Main section -->
+    <main>
+
+    <p>
+        We provide neat trips to Mars that's so hard to get. Once in a lifetime chance, so don't miss out!
+    </p>
+
+    </main>
+
+    <footer>
+
+
+    </footer>
+</body>
+</html>
